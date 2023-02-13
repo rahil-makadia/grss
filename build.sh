2f7ed587eed6cbe27686c9dfa4200d39bd0c26f7
Rahil Makadia <makadia2@illinois.edu>
2023-02-12 23:39:28 -0600

    updated build script and environment yml

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
-#!/bin/zsh
+#!/bin/bash
+
+clean=0
+for i in $*; do
+    if [[ $i == "-clean" ]]; then
+        clean=1
+    fi
+done
 
 # This script is used to build the project.
-rm -r build
-mkdir build
-cd build
-cmake .. -DPYBIND11_FINDPYTHON=ON
-make
-rm ../examples/*.so
-cp *.so ../examples/
+if [[ $clean -eq 1 ]]; then
+    rm -r build
+    mkdir build
+    cd build
+    cmake .. -DPYBIND11_FINDPYTHON=ON
+    make
+    rm ../examples/*.so
+    cp *.so ../examples/
+else
+    cd build
+    make
+    rm ../examples/*.so
+    cp *.so ../examples/
+fi
