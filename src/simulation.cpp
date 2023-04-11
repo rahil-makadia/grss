c4c3d1006d0268617871965d43560012f5c8c89b
Rahil Makadia <makadia2@illinois.edu>
2023-04-11 00:11:47 -0500

    real present obs + no noise future obs working?

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -627,7 +627,7 @@ void propSimulation::extend(real tf, std::vector<real> tEvalNew, std::vector< st
     // std::reverse(this->xIntegEval.begin(), this->xIntegEval.end());
     // std::reverse(this->radarObsEval.begin(), this->radarObsEval.end());
 
-    std::cout << "WARNING: The extend() function is under development and may not work properly." << std::endl;
+    std::cout << "WARNING: The extend() method is under development and may not work properly with the interpolation routines." << std::endl;
 
     // empty existing vectors from previous integration
     this->tEval.clear();
