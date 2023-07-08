3f3fc1c46154b1dc14a8a57081e709ee2c2fff60
Rahil Makadia <makadia2@illinois.edu>
2023-07-07 23:32:52 -0700

    separated linux/mac python build processes to fix missing symbols

--- build_python.sh
+++ build_python.sh
@@ -4,8 +4,23 @@ rm -rf build dist grss.egg-info
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade build twine cibuildwheel
 
-python3 -m build --sdist
-python3 -m cibuildwheel --output-dir dist
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    MSYS_NT*)   machine=Git;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+python3 -m build --sdist --outdir dist
+if [ $machine = "Linux" ]; then
+    python3 -m cibuildwheel --output-dir dist
+fi
+if [ $machine = "Mac" ]; then
+    python3 -m build --wheel --outdir dist
+fi
 
 # if file run with -pypi then upload to pypi, else upload to testpypi
 twine check dist/*
