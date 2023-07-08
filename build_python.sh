205007056a4531e49b388ae8e9e754587399cf94
Rahil Makadia <makadia2@illinois.edu>
2023-07-08 12:23:08 -0700

    updated cibuildwheel workflow for macs

--- build_python.sh
+++ build_python.sh
@@ -4,23 +4,8 @@ rm -rf build dist grss.egg-info
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade build twine cibuildwheel
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    MSYS_NT*)   machine=Git;;
-    *)          machine="UNKNOWN:${unameOut}"
-esac
-
 python3 -m build --sdist --outdir dist
-if [ $machine = "Linux" ]; then
-    python3 -m cibuildwheel --output-dir dist
-fi
-if [ $machine = "Mac" ]; then
-    python3 -m build --wheel --outdir dist
-fi
+python3 -m cibuildwheel --output-dir dist
 
 # if file run with -pypi then upload to pypi, else upload to testpypi
 twine check dist/*
