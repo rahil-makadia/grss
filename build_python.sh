701b3caa88a5c0feafb4192a26b6e9ec49715ab5
Rahil Makadia <makadia2@illinois.edu>
2023-07-07 18:39:58 -0700

    updated python build script for linux wheel pypi upload

--- build_python.sh
+++ build_python.sh
@@ -2,9 +2,10 @@
 rm -rf build dist grss.egg-info
 
 python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade build twine
+python3 -m pip install --upgrade build twine cibuildwheel
 
-python3 -m build --sdist --wheel
+python3 -m build --sdist
+python3 -m cibuildwheel --output-dir dist
 
 # if file run with -pypi then upload to pypi, else upload to testpypi
 twine check dist/*
