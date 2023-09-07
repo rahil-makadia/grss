3daa5abb9d704f714cd43cf0eee3f56e5a206433
Rahil Makadia <makadia2@illinois.edu>
2023-09-07 02:12:39 -0500

    no more codecov

--- tests/python/run_python_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf .pytest_cache
-rm -rf fit/__pycache__
-rm -rf prop/__pycache__
-jupyter nbconvert --to script fit/*.ipynb
-jupyter nbconvert --to script prop/*.ipynb
-pytest
-rm -rf fit/*.py
-rm -rf prop/*.py
