c7d91136ac4f2669d5222a7b6db1297cdb6c5230
Rahil Makadia <makadia2@illinois.edu>
2023-12-24 01:28:24 -0800

    added central body arg to ca/impact clustering

--- grss/prop/prop_parallel.py
+++ grss/prop/prop_parallel.py
@@ -1,19 +1,21 @@
 """Parallel computing utilities for the GRSS orbit propagation code"""
 import pandas as pd
 
-__all__ = [ 'cluster_approaches',
+__all__ = [ 'cluster_ca_or_impacts',
 ]
 
-def cluster_approaches(ca_list, max_duration=45):
+def cluster_ca_or_impacts(full_list, max_duration=45, central_body=399):
     """
     Cluster a list of close approaches by time and uniqueness.
 
     Parameters
     ----------
-    ca_list : list of prop_simulation.CloseApproachParameters objects
+    full_list : list of prop_simulation.CloseApproachParameters objects
         List of close approaches to cluster.
     max_duration : float
         Maximum duration (in days) between close approaches in a cluster.
+    central_body : int
+        SPICE ID of the central body.
 
     Returns
     -------
@@ -22,9 +24,13 @@ def cluster_approaches(ca_list, max_duration=45):
         close approaches).
     """
     all_clusters = []
-    times = [ca.t for ca in ca_list]
-    bodies = [ca.flybyBody for ca in ca_list]
-    idx_list = list(range(len(ca_list)))
+    full_list = [ca_or_impact for ca_or_impact in full_list
+                    if ca_or_impact.centralBodySpiceId == central_body]
+    if not full_list:
+        return tuple(all_clusters)
+    times = [ca_or_impact.t for ca_or_impact in full_list]
+    bodies = [ca_or_impact.flybyBody for ca_or_impact in full_list]
+    idx_list = list(range(len(full_list)))
 
     df = pd.DataFrame({'time': times, 'body': bodies, 'idx': idx_list})
     df = df.sort_values(by=['time'])
@@ -33,17 +39,17 @@ def cluster_approaches(ca_list, max_duration=45):
     # 1. the time between the current close approach and the last close approach
     #       is greater than max_duration
     # 2. the current close approach body is already in the current cluster
-    cluster = [ca_list[df.iloc[0]['idx']]]
+    cluster = [full_list[df.iloc[0]['idx']]]
     cluster_bodies = [df.iloc[0]['body']]
     for i in range(1, len(df)):
         time_condition = df.iloc[i]['time'] - df.iloc[i-1]['time'] > max_duration
         body_condition = df.iloc[i]['body'] in cluster_bodies
         if time_condition or body_condition:
             all_clusters.append(cluster)
-            cluster = [ca_list[df.iloc[i]['idx']]]
+            cluster = [full_list[df.iloc[i]['idx']]]
             cluster_bodies = [df.iloc[i]['body']]
         else:
-            cluster.append(ca_list[df.iloc[i]['idx']])
+            cluster.append(full_list[df.iloc[i]['idx']])
             cluster_bodies.append(df.iloc[i]['body'])
     all_clusters.append(cluster)
     return tuple(all_clusters)
