064b8a8c061f572a3b75ae24b290827725f0c89b
Rahil Makadia <makadia2@illinois.edu>
2024-01-22 20:31:32 -0600

    update conf.py for docs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import grss
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
