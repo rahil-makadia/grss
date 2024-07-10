b035bc9c8e693590c2f6848d0d8ebf218be58838
Rahil Makadia <makadia2@illinois.edu>
2024-07-10 15:09:41 -0700

    correct directory for initialize script

--- initialize.sh
+++ initialize.sh
@@ -4,9 +4,9 @@
 pip3 install "pybind11[global]>=2.10.0"
 
 # get debiasing data and kernels
-cd ../grss/debias/
+cd grss/debias/
 python3 get_debiasing_data.py
-cd ../kernels/
+cd kernels/
 python3 get_kernels.py
 
 # return to root
