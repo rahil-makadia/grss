f05a1b82045db47f77277391a572f2ed6137b8a5
Rahil Makadia <makadia2@illinois.edu>
2024-07-10 15:11:39 -0700

    path cleanup for tests

--- initialize.sh
+++ initialize.sh
@@ -6,7 +6,7 @@ pip3 install "pybind11[global]>=2.10.0"
 # get debiasing data and kernels
 cd grss/debias/
 python3 get_debiasing_data.py
-cd kernels/
+cd ../kernels/
 python3 get_kernels.py
 
 # return to root
