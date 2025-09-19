fb5631295f03026265f6bf6b17173c66decd071c
Rahil Makadia <makadia2@illinois.edu>
2025-09-19 12:00:30 -0700

    go back to old kernel downloaders

--- grss/debias/get_debiasing_data.py
+++ grss/debias/get_debiasing_data.py
@@ -1,32 +1,21 @@
 """Download the debiasing data files from the JPL Solar System Dynamics Group's FTP server"""
 import os
-import requests
-
-def check_and_download(url, local_path, note):
-    try:
-        response = requests.head(url, timeout=30, allow_redirects=True)
-        if response.status_code == 200:
-            print(f'Downloading {note}...')
-            response = requests.get(url, timeout=30)
-            with open(local_path, 'wb') as f:
-                f.write(response.content)
-        else:
-            msg = f'File {url} ({note}) does not exist on server. Code: {response.status_code}'
-            raise FileNotFoundError(msg)
-    except requests.RequestException as e:
-        raise ConnectionError(f'Error checking/downloading {note}') from e
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
-FTP_SITE = 'https://ssd.jpl.nasa.gov/ftp/ssd/debias'
+FTP_SITE = 'ftp://ssd.jpl.nasa.gov/pub/ssd/debias'
 # get debiasing data files from the JPL Solar System Dynamics Group's FTP server
 # if lowres_data.tgz or lowres_data/ already exist, do not download
 if not os.path.exists(f'{cwd}/lowres_data.tgz') and not os.path.exists(f'{cwd}/lowres_data/'):
-    check_and_download(f'{FTP_SITE}/debias_2018.tgz',
-                        f'{cwd}/lowres_data.tgz', 'low-resolution debiasing data')
+    FTP_FILE = 'debias_2018.tgz'
+    cmd = f'curl --connect-timeout 30 --show-error -o {cwd}/lowres_data.tgz {FTP_SITE}/{FTP_FILE}'
+    print('Downloading low-resolution debiasing data...')
+    os.system(cmd)
 if not os.path.exists(f'{cwd}/hires_data.tgz') and not os.path.exists(f'{cwd}/hires_data/'):
-    check_and_download(f'{FTP_SITE}/debias_hires2018.tgz',
-                        f'{cwd}/hires_data.tgz', 'high-resolution debiasing data')
+    FTP_FILE = 'debias_hires2018.tgz'
+    cmd = f'curl --connect-timeout 30 --show-error -o {cwd}/hires_data.tgz {FTP_SITE}/{FTP_FILE}'
+    print('Downloading high-resolution debiasing data...')
+    os.system(cmd)
 
 # extract the data files
 # create the directories if they don't exist, delete the files if they do
--- grss/kernels/get_kernels.py
+++ grss/kernels/get_kernels.py
@@ -2,23 +2,6 @@
 GRSS GitHub repository and the NAIF FTP server"""
 import os
 import time
-import requests
-
-def check_and_download(url, local_path, note, force=False):
-    if os.path.exists(local_path) and not force:
-        return
-    try:
-        response = requests.head(url, timeout=30, allow_redirects=True)
-        if response.status_code == 200:
-            print(f'Downloading {note}...')
-            response = requests.get(url, timeout=30)
-            with open(local_path, 'wb') as f:
-                f.write(response.content)
-        else:
-            msg = f'File {url} ({note}) does not exist on server. Code: {response.status_code}'
-            raise FileNotFoundError(msg)
-    except requests.RequestException as e:
-        raise ConnectionError(f'Error checking/downloading {note}') from e
 
 # get the path to the directory containing this script
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -28,37 +11,61 @@ SSD_SITE = 'https://ssd.jpl.nasa.gov/ftp'
 
 # get the spice kernels if they are not already present
 # de430 planets + de431 big16
-check_and_download(f'{NAIF_SITE}/spk/planets/de430.bsp',
-                    f'{script_dir}/de430.bsp', 'DE430 planet kernel')
-check_and_download(f'{SSD_SITE}/xfr/sb431-n16s.bsp',
-                    f'{script_dir}/sb431-n16s.bsp', 'DE431 big16 asteroid kernel')
-# de440/441 planets + de441 big16
-check_and_download(f'{SSD_SITE}/eph/planets/bsp/de440.bsp',
-                    f'{script_dir}/de440.bsp', 'DE440 planet kernel')
-check_and_download(f'{SSD_SITE}/xfr/sb441-n16s.bsp',
-                    f'{script_dir}/sb441-n16s.bsp', 'short-term DE441 big16 asteroid kernel')
-check_and_download(f'{SSD_SITE}/eph/planets/bsp/de441.bsp',
-                    f'{script_dir}/de441.bsp', 'DE441 planet kernel')
-check_and_download(f'{SSD_SITE}/eph/small_bodies/asteroids_de441/sb441-n16.bsp',
-                    f'{script_dir}/sb441-n16.bsp', 'long-term DE441 big16 asteroid kernel')
+mb430_exists = os.path.exists(f'{script_dir}/de430.bsp')
+sb431_exists = os.path.exists(f'{script_dir}/sb431-n16s.bsp')
+if (not mb430_exists or not sb431_exists):
+    print('Downloading DE430/431 planet and big16 asteroid kernels...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/de430.bsp '
+                f'{NAIF_SITE}/spk/planets/de430.bsp'))
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/sb431-n16s.bsp '
+                f'{SSD_SITE}/xfr/sb431-n16s.bsp'))
+# de440 planets + de441 big16
+mb440_exists = os.path.exists(f'{script_dir}/de440.bsp')
+sb441s_exists = os.path.exists(f'{script_dir}/sb441-n16s.bsp')
+mb441_exists = os.path.exists(f'{script_dir}/de441.bsp')
+sb441_exists = os.path.exists(f'{script_dir}/sb441-n16.bsp')
+if (not mb440_exists or not mb441_exists or not sb441s_exists or not sb441_exists):
+    print('Downloading DE440/441 planet and big16 asteroid kernels...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/de440.bsp '
+                f'{SSD_SITE}/eph/planets/bsp/de440.bsp'))
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/sb441-n16s.bsp '
+                f'{SSD_SITE}/xfr/sb441-n16s.bsp'))
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/de441.bsp '
+                f'{SSD_SITE}/eph/planets/bsp/de441.bsp'))
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/sb441-n16.bsp '
+                f'{SSD_SITE}/eph/small_bodies/asteroids_de441/sb441-n16.bsp'))
 
+# get the earth orientation binary spice kernels and their comments if they are not already present
 # latest earth pck
 latest_earth_pck_exists = os.path.exists(f'{script_dir}/earth_latest.bpc')
 latest_earth_pck_old = (latest_earth_pck_exists and
                         time.time() - os.path.getmtime(f'{script_dir}/earth_latest.bpc')
                         > 86400)
 if not latest_earth_pck_exists or latest_earth_pck_old:
-    check_and_download(f'{NAIF_SITE}/pck/earth_latest_high_prec.bpc',
-                       f'{script_dir}/earth_latest.bpc', 'latest Earth binary PCK', force=True)
+    print('Downloading latest Earth binary PCK...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/earth_latest.bpc '
+                f'{NAIF_SITE}/pck/earth_latest_high_prec.bpc'))
 # historical earth pck
-check_and_download(f'{NAIF_SITE}/pck/earth_620120_250826.bpc',
-                    f'{script_dir}/earth_historic.bpc', 'historic Earth binary PCK')
+historical_earth_pck_exists = os.path.exists(f'{script_dir}/earth_historic.bpc')
+if not historical_earth_pck_exists:
+    print('Downloading historic Earth binary PCK...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/earth_historic.bpc '
+                f'{NAIF_SITE}/pck/earth_620120_250826.bpc'))
 # predicted earth pck
-check_and_download(f'{NAIF_SITE}/pck/earth_2025_250826_2125_predict.bpc',
-                    f'{script_dir}/earth_predict.bpc', 'predicted Earth binary PCK')
+predicted_earth_pck_exists = os.path.exists(f'{script_dir}/earth_predict.bpc')
+if not predicted_earth_pck_exists:
+    print('Downloading predicted Earth binary PCK...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/earth_predict.bpc '
+                f'{NAIF_SITE}/pck/earth_2025_250826_2125_predict.bpc'))
 # moon pck
-check_and_download(f'{NAIF_SITE}/pck/moon_pa_de440_200625.bpc',
-                    f'{script_dir}/moon_pa_de440.bpc', 'Moon PCK for DE440')
+moon_pa_de440_exists = os.path.exists(f'{script_dir}/moon_pa_de440.bpc')
+if not moon_pa_de440_exists:
+    print('Downloading Moon PCK for DE440...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/moon_pa_de440.bpc '
+                f'{NAIF_SITE}/pck/moon_pa_de440_200625.bpc'))
 # generic frame kernels
-check_and_download(f'{NAIF_SITE}/pck/pck00011.tpc',
-                    f'{script_dir}/pck00011.tpc', 'generic frame kernel')
+generic_frame_kernel_exists = os.path.exists(f'{script_dir}/pck00011.tpc')
+if not generic_frame_kernel_exists:
+    print('Downloading generic frame kernel...')
+    os.system((f'curl --connect-timeout 30 --show-error -o {script_dir}/pck00011.tpc '
+                f'{NAIF_SITE}/pck/pck00011.tpc'))
