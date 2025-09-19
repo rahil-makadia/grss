c1f42adc9a833bbad8689fc887ef711a8d48ae80
Rahil Makadia <makadia2@illinois.edu>
2025-09-19 15:36:04 -0700

    github runner CI issue diagnosis pt.16

--- tests/python/fit/a_CI_test.py
+++ tests/python/fit/a_CI_test.py
@@ -67,6 +67,6 @@ else:
 # read in the data and add extra columns if not present
 obs_df = pd.read_xml(obs_data, dtype=ades_column_types)
 
-# obs_times = Time(obs_df['obsTime'].to_list(), format='isot', scale='utc')
+obs_times = Time(obs_df['obsTime'].to_list(), format='isot', scale='utc')
 # obs_df['obsTimeMJD'] = obs_times.utc.mjd
 # obs_df['obsTimeMJDTDB'] = obs_times.tdb.mjd
