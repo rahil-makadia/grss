79d1ac41af75d1809f0e95016464bd1563d3435b
Rahil Makadia <40069449+rahil-makadia@users.noreply.github.com>
2026-01-23 12:25:23 -0800

    better use of MPC obscodes API for special sites

--- grss/__init__.py
+++ grss/__init__.py
@@ -1,9 +1,8 @@
 """GRSS: the Gauss-Radau Small-body Simulator"""
-from . import fit
-from . import prop
 from . import utils
-
 # get version from version.txt
 __version__ = open(f'{utils.grss_path}/version.txt', 'r', encoding='utf-8').read().strip()
-
 utils.initialize()
+
+from . import fit
+from . import prop
